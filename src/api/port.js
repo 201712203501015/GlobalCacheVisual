7f73a73d7b2de6e1a280550d6e614aca070f5ef9
Faker0715 <949506563@qq.com>
2023-04-13 02:35:15 +0800

    [fix] change port to configuration

--- src/api/port.js
+++ src/api/port.js
@@ -1,8 +1,4 @@
-// axios接口端口号 9999
-export const AJAX_PORT = '8899';
-// websocket接口端口号 9998
+export const AJAX_PORT = '9999';
 export const WEBSOCKET_PORT = '9998';
-// websocket的后端公网IP 175.34.8.36
-export const IP = "localhost:";
-// axios的接口IP http://175.34.8.36:
-export const AJAX_IP = 'http://localhost:';
+export const IP = "0.0.0.0:";
+export const AJAX_IP = 'http://0.0.0.0:';
