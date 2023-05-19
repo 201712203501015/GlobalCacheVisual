4a646ac39e463a723aa8dc31d81033f927f02a3a
zxy201712203501015 <1872698892@qq.com>
2023-05-19 17:59:01 +0800

    自动化部署分步

--- src/components/install/install.vue
+++ src/components/install/install.vue
@@ -4,14 +4,17 @@
     <div class="install-top">
       <h3>安装日志信息</h3>
     </div>
+    <!-- 当前安装完的步骤 -->
     <div class="install-title">
-      安装步骤
+      <div id="sroll1" class="infinite-list1" style="overflow: auto;">
+        <p class="text-wrapper" v-for="(item,index) in logInfoList" :key="index" style="height: 20px;">{{ item }}</p>
+      </div>
     </div>
+    <!-- 安装细节 -->
     <div class="install-box">
       <!-- <div id="sroll" class="infinite-list" style="overflow: auto;flex-grow:2;overflow-y:auto;overflow-x:hidden;">
         <p class="text-wrapper" v-for="(item,index) in logInfoList" :key="index" style="height: 20px;">{{ item }}</p>
       </div> -->
-      <!-- 测试，自动换行 -->
       <div id="sroll" class="infinite-list" style="overflow-y:auto;overflow-x:none;white-space: pre;">
         {{ strcontent }}
       </div>
@@ -19,8 +22,11 @@
         <b>恭喜你安装成功</b>
       </span> -->
     </div>
-    <div v-if="isSucceed" style="margin-top: 10px; align-items: center;justify-content: center;display: flex;">
-      <el-button type="primary" @click="goto('userView')">完成</el-button>
+    <div style="margin-top: 10px; align-items: center;justify-content: center;display: flex;">
+      <!-- 下一步按钮 -->
+      <el-button v-if="isSucceed===false && nowEnd===true" type="primary" @click="Start()">下一步</el-button>
+      <!-- 完成按钮 -->
+      <el-button v-if="isSucceed" type="primary" @click="goto('userView')">完成</el-button>
     </div>
   </div>
 </template>
@@ -34,15 +40,17 @@ export default {
   setup(){
     // 创建store对象
     const store = useStore()
+    // 创建步骤集合
+    const stepSet = new Set();
     return {
-      store,
+      store,stepSet
     }
   },
   data() {
     return {
       // 时间戳
       timeId: null,
-      // 日志信息
+      // 记录安装步骤---名称
       logInfoList: [],
       // 安装成功
       isSucceed: false,
@@ -50,6 +58,10 @@ export default {
       sroll: null,
       // 字符串信息
       strcontent: "",
+      // 第几步
+      nowStep: 0,
+      // 第几步是否安装完毕,true状态下是禁用 下一步 按钮
+      nowEnd: false,
     }
   },
   mounted() {
@@ -71,8 +83,6 @@ export default {
     },
     // 像后端发送安装请求
     sendInstall() {
-      this.strcontent += "正在请求安装......\n"
-      this.logInfoList.push('正在请求安装......')
       API({
         url: '/getBeginInstall',
         method: 'post',
@@ -83,8 +93,6 @@ export default {
         let recvdata = res.data.data
         // 可以开始了，像后端建立连接
         if(recvdata.isBegin === true) {
-          this.strcontent += "***** 开始安装 *****\n"
-          this.logInfoList.push('***** 开始安装 *****')
           this.Start();
         }
       }).catch(err => {
@@ -147,36 +155,61 @@ export default {
       }
     },
     Start() {
+      console.log("调用了start")
+      // 关闭下一步按钮
+      this.nowEnd = false
       // 每隔3s向后端请求一次
       this.timeId = setInterval(() => {
         API({
           url: '/getStartInstall',
           method: 'post',
           data: {
-            token: this.store.state.userToken,
+           token: this.store.state.userToken, 
+           nowStep: this.nowStep, // 当前是第几步
+           nowEnd: this.nowEnd, // 第几步是否结束
           }
         }).then((res) => {
           let recvdata = res.data.data
-          // this.logInfoList.push(recvdata.installLogInfo)
-          // this.PushInfo(recvdata.installLogInfo) // 5-19 弃用
-          this.strcontent += recvdata.installLogInfo // 更新字符串
-          // 字符串长度小于100000，防止渲染不了
-          if(this.strcontent.length > 100000)
-          {
-            this.strcontent = this.strcontent.substring(-100000);
+          if(this.stepSet.has(recvdata.nowStep)===false) { // 新的一步
+            this.stepSet.add(recvdata.nowStep);
+            this.logInfoList.push(recvdata.nowName);
+            this.nowStep = recvdata.nowStep; // 第几步(当前)
+            // 更新显示内容
+            this.strcontent = "" // 清空数组
+            this.strcontent += "正在请求安装......\n"
+            this.strcontent += "***** 开始安装 *****\n"
           }
-          nextTick(() => {
-            let sroll = document.getElementById('sroll');
-            sroll.scrollTop = sroll.scrollHeight
-            // console.log('----',sroll.scrollHeight)
-          })
-          if(recvdata.isEnd === true) {
-            // 等5s后再跳转
-            this.sleep1(5000,()=>{
-              this.isSucceed = true
+
+          // 和当前step 一样
+          if(this.nowStep === recvdata.nowStep) {
+            this.strcontent += recvdata.installLogInfo // 更新字符串
+            // 字符串长度小于100000，防止渲染不了
+            if(this.strcontent.length > 100000)
+            {
+              this.strcontent = this.strcontent.substring(-100000);
+            }
+            // 重定位
+            nextTick(() => {
+              let sroll = document.getElementById('sroll');
+              sroll.scrollTop = sroll.scrollHeight
+              let sroll1 = document.getElementById('sroll1');
+              sroll1.scrollTop = sroll1.scrollHeight
             })
-            // this.logInfoList.push('***** 安装结束 *****')
+            // (1) 整体安装是否完毕?
+            if(recvdata.isEnd === true) {
+              // 等5s后再跳转,显示 安装完毕
+              this.sleep1(5000,()=>{
+                this.isSucceed = true
+              })
+            }
+            // (2) 第step步是否安装完毕,显示安装下一步
+            if(recvdata.nowEnd === true) {
+              this.nowEnd = true // 开启 下一步 按钮
+              console.log("当前nowEnd = ",this.nowEnd);
+              clearInterval(this.timeId); // 销毁定时器
+            }
           }
+          
         })
       },1000)
     }
@@ -188,6 +221,14 @@ export default {
 .install-com {
   margin: 10px;
 }
+
+.infinite-list1 {
+  height: 140px;
+  width: 90%;
+  word-break: break-all;
+  word-wrap: break-word;
+}
+
 .infinite-list {
   height: 280px;
   width: 90%;
